d5cc72c9278876efa51c43a7afed003f46833629
maheshwar reddy <maheshwarreddy.c1729@gmail.com>
2019-04-02 22:06:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ This program can handle any number of price lists (operators) and then
 calculate which operator is cheapest for a certain number."""
-
 # Here we are using a regular expression (re) module which is a special sequence
 # of characters that helps to match or find other strings or sets of strings.
 import re
-
 def make_dicts(file_input):
     """To make a dictionary with prefix as key and price as value"""
     with open(file_input) as file_1:
@@ -15,7 +13,6 @@ def make_dicts(file_input):
 			# Here we are spliting the phone numbers and prices from a file.
             price_list[prefix] = float(price)
     return price_list
-
 def match_num(price_list, phone_num):
     """ Matching the phone number with the prefix from the price list"""
     out = {}
@@ -25,7 +22,6 @@ def match_num(price_list, phone_num):
             out = {}
             out[prefix] = price_list[prefix]
     return out
-
 def operator_name(match_a, match_b):
     """ Calculating which operator is cheapest"""
     if not match_a:
@@ -34,40 +30,29 @@ def operator_name(match_a, match_b):
         return "B"
     if not match_b:
         return "A"
-
     for key in match_a:
         final_a = match_a[key]
-
     for key in match_b:
         final_b = match_b[key]
-
     if final_a > final_b:
         final_op = "B"
     else:
         final_op = "A"
-
     return final_op
-
 def main():
     """To determine the cheapest operator for the given number"""
     price_a = make_dicts("A_op.txt")
     price_b = make_dicts("B_op.txt")
-
     phone_num = input("Enter phone number: ")
-
     for k in phone_num:
         if k in ('+', '-'):
             phone_num = phone_num.replace(k, "")
-
     match_a = match_num(price_a, phone_num)
     print("match A: ", match_a)
-
     match_b = match_num(price_b, phone_num)
     print("match B: ", match_b)
     print("\n")
-
     final_op = operator_name(match_a, match_b)
     print("cheapest operator : ", final_op)
-
 if __name__ == "__main__":
     main()
